93cb83269ecb0f5e62d824a4010db47892f08549
TechDudie <73961295+TechDudie@users.noreply.github.com>
2022-02-07 12:15:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <title>Bash</title>
+    <title>TechnoQuiz</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Ubuntu Mono'>
   </head>
   <body>
     <textarea autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" id="terminal" class="terminal" rows="1000" cols="1000"></textarea>
     <script src="lib/lz-string.js"></script>
-    <script src="js/commands.js"></script>
-    <script src="js/functions.js"></script>
     <script src="js/main.js"></script>
   </body>
 </html>
