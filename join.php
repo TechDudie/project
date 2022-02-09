e53262e3b886807cddf02aadbfae917cffd1da6c
TechDudie <73961295+TechDudie@users.noreply.github.com>
2022-02-08 18:50:47 -0600

    Update join.php

--- join.php
+++ join.php
@@ -2,17 +2,20 @@
 $code = htmlentities($_POST["code"]);
 if ($code != "314159") { // PIE IN YOUR FACE LITERALLY IF YOU READ THE JOIN CODE
   header('Location: 404.html');
+  die(); // LEGENDS NEVER DIEEEEE
+  // clearly this isn't a legend then
 }
+header('Location: game.html');
 ?>
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <title>Game</title>
+    <title>Connecting</title>
     <link rel="stylesheet" href="css/game.css">
     <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Ubuntu Mono'>
   </head>
   <body>
-    <textarea autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" id="terminal" class="terminal" rows="1000" cols="1000"></textarea>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <h1>Authenicating...</h1>
+    <h2>You are being redirected to the correct location.</h2>
   </body>
 </html>
